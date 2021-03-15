752d009b6d858afadf942483022c46c1415fca8b
jn <jn@192.168.1.6>
2021-03-15 20:02:11 +0200

    New commit

--- src/mybookwishlist/CaptureBook.java
+++ src/mybookwishlist/CaptureBook.java
@@ -6,8 +6,6 @@
 package mybookwishlist;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
